9d9005dedf8e0adb90e646cdb681cb87b4a01482
atapadar12 <amrita.tapadar12@gmail.com>
2024-10-05 00:11:13 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+#strong concatenation (how to put strings together)
+#lets go with a default string "thank you for -------"
+
+adj = input("Adjective :")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+madlib = f"today i am {verb1} to have a {adj} day doing {verb2}"
+
+print(madlib)
