4929b60a1c36a4280c99c63bd92bfaacbf414361
atapadar12 <amrita.tapadar12@gmail.com>
2024-10-04 18:24:19 +0530

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,13 @@
+temp = 75
+forecast = "sunny"
+if not forecast == "rainy":
+    print("go outside")
+else:
+    print("stay indoors") 
+
+raining = True
+if not raining:
+    print("go outdoor") 
+else:
+    print("stay inside")  
+    
